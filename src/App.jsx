12b2b707d86cb78a4ccb77fe7b08134c695845c9
danajacobc <danajacobc@gmail.com>
2023-10-26 10:04:12 -0300

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Landing from './components/Landing'
+import Home from './components/Home'
+import Favorites from './components/Favorites'
+import Detail from './components/Detail'
+import NavBar from './components/NavBar'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+	const {pathname} = useLocation(); //destructuro pathname de useLocation, que nos muestra "donde estamos" ej: /
+  
   return (
 		<div>
-			<h1>Repaso Módulo 2</h1>
-			<hr />
-			<h2>Cohorte FT44a</h2>
-			<h2>Repaso de: danajacobc</h2>
-			<h2>Rick & Morty - Episodes</h2>
-			<h3>Ver README.md para ver detalles del proyecto</h3>
+			{pathname !== "/" ? <NavBar /> : null}
+			<Routes>
+				<Route path="/" element={<Landing />}/>
+				<Route path="/home" element={<Home />}/>
+				<Route path="/favorites" element={<Favorites />}/>
+				<Route path="/detail/:id" element={<Detail />}/>
+				<Route path="*" element={<Landing />}/>
+			</Routes>
 		</div>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function NavBar(props) {
   return (
     <div>
-      <h2>NavBar</h2>
+      <span>Rick&Morty Episodes</span>
+      <br />
+      <br />
+      <button>
+        <NavLink to="/Home"
+        style={({isActive}) => isActive ? {color: "coral"} : {color: "grey"}}
+        >Home</NavLink>
+      </button>
+
+
+      <button>
+        <NavLink to="/Favorites"
+        style={({isActive}) => isActive ? {color: "coral"} : {color: "grey"}}
+        >Favorites</NavLink>
+      </button>
     </div>
   );
 }
